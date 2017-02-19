81bf9b7b2d0d70ade48f7b04c8a05fa0146ae889
Bruno Kosawa <bkosawa@gmail.com>
2017-02-19 18:37:15 -0300

    Fixing base url in Service generator

--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/retrofit/ServiceGenerator.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/retrofit/ServiceGenerator.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-    private static String API_BASE_URL = BuildConfig.BASE_URL;
+    private static String API_BASE_URL = BuildConfig.BASE_URL + "api/";
 
     private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
@@ -22,7 +22,7 @@ public class ServiceGenerator {
             new HttpLoggingInterceptor()
                     .setLevel(HttpLoggingInterceptor.Level.BODY);
 
-    private static Retrofit retrofit = builder.client(httpClient.build()).build();
+    private static Retrofit retrofit;
 
     public static <S> S createService(Class<S> serviceClass) {
         return createService(serviceClass, null);
