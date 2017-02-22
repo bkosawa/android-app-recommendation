e57d28da0baf7440ff027894937cd14723dabec2
Bruno Kosawa <bkosawa@gmail.com>
2017-02-22 08:48:08 -0300

    Adding body in login request

---
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/pojo/Login.java
@@ -0,0 +1,11 @@
+package br.com.kosawalabs.apprecommendation.data.pojo;
+
+public class Login {
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
