24e350cf07dbfb915c0b548d089d504f22412d42
Bruno Kosawa <bkosawa@gmail.com>
2017-02-23 09:49:06 -0300

    Adding request in AppRecommendationClient to get detail of an id

--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/retrofit/service/AppRecommendationClient.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/retrofit/service/AppRecommendationClient.java
@@ -1,12 +1,17 @@
 package br.com.kosawalabs.apprecommendation.data.network.retrofit.service;
 
+import br.com.kosawalabs.apprecommendation.data.pojo.App;
 import br.com.kosawalabs.apprecommendation.data.pojo.AppResult;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface AppRecommendationClient {
     @GET("/api/apps/")
     Call<AppResult> getApps(@Query("offset") Long offset,
                             @Query("limit") Long limit);
+
+    @GET("/api/apps/{appid}")
+    Call<App> getApp(@Path("appid") Integer appId);
 }
