ec1670373c6a6776bacda2337febf2181def32f8
Bruno Kosawa <bkosawa@gmail.com>
2017-02-23 09:37:17 -0300

    Creating method to start AppList with token

--- app/src/main/java/br/com/kosawalabs/apprecommendation/AppListActivity.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/AppListActivity.java
@@ -1,5 +1,6 @@
 package br.com.kosawalabs.apprecommendation;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,33 +8,26 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
 
 import br.com.kosawalabs.apprecommendation.dummy.DummyContent;
 
-import java.util.List;
-
-/**
- * An activity representing a list of Apps. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link AppDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class AppListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
+    public static final String EXTRAS_SESSION_TOKEN = "br.com.kosawalabs.apprecommendation.extras.SESSION_TOKEN";
     private boolean mTwoPane;
+    private String token;
+
+    public static void start(Activity activity, String token) {
+        Intent intent = new Intent(activity, AppListActivity.class);
+        intent.putExtra(EXTRAS_SESSION_TOKEN, token);
+        activity.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +38,15 @@ public class AppListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        View recyclerView = findViewById(R.id.app_list);
+        final View recyclerView = findViewById(R.id.app_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
 
         if (findViewById(R.id.app_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
             mTwoPane = true;
         }
+
+        token = getIntent().getStringExtra(EXTRAS_SESSION_TOKEN);
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
