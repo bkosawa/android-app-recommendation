ddfdd1a5b18f635f3ecc6c88961f00093e490420
Bruno Kosawa <bkosawa@gmail.com>
2017-03-02 08:54:31 -0300

    Fixing test on Method that gets first page of apps

--- app/src/main/java/br/com/kosawalabs/apprecommendation/presentation/AppListPresenter.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/presentation/AppListPresenter.java
@@ -1,7 +1,12 @@
 package br.com.kosawalabs.apprecommendation.presentation;
 
 
+import java.util.List;
+
 import br.com.kosawalabs.apprecommendation.data.AppDataRepository;
+import br.com.kosawalabs.apprecommendation.data.DataCallback;
+import br.com.kosawalabs.apprecommendation.data.DataError;
+import br.com.kosawalabs.apprecommendation.data.pojo.App;
 
 public class AppListPresenter {
     private AppDataRepository repository;
@@ -11,6 +16,16 @@ public class AppListPresenter {
     }
 
     public void fetchFirstPage() {
+        repository.getApps(0L, 0L, new DataCallback<List<App>>() {
+            @Override
+            public void onSuccess(List<App> result) {
 
+            }
+
+            @Override
+            public void onError(DataError error) {
+
+            }
+        });
     }
 }
