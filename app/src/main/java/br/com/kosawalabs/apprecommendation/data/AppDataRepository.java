bf9796a54061523dab8b2c4f99d76b5fcf6abee4
Bruno Kosawa <bkosawa@gmail.com>
2017-03-12 17:18:39 -0300

    Creating methods in repository to get and send recommended apps

--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/AppDataRepository.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/AppDataRepository.java
@@ -3,9 +3,14 @@ package br.com.kosawalabs.apprecommendation.data;
 import java.util.List;
 
 import br.com.kosawalabs.apprecommendation.data.pojo.App;
+import br.com.kosawalabs.apprecommendation.data.pojo.PackageName;
 
 public interface AppDataRepository {
     void getApps(Long offset, Long limit, DataCallback<List<App>> callback);
 
     void getApp(Integer appId, DataCallback<App> callback);
+
+    void getRecommendedApps(Long offset, Long limit, DataCallback<List<App>> callback);
+
+    void sendMyApps(List<PackageName> packageNames, DataCallback<Void> callback);
 }
--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/AppNetworkRepository.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/network/AppNetworkRepository.java
@@ -9,6 +9,7 @@ import br.com.kosawalabs.apprecommendation.data.network.retrofit.ServiceGenerato
 import br.com.kosawalabs.apprecommendation.data.network.retrofit.service.AppRecommendationClient;
 import br.com.kosawalabs.apprecommendation.data.pojo.App;
 import br.com.kosawalabs.apprecommendation.data.pojo.AppResult;
+import br.com.kosawalabs.apprecommendation.data.pojo.PackageName;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -42,7 +43,8 @@ public class AppNetworkRepository implements AppDataRepository {
 
             @Override
             public void onFailure(Call<AppResult> call, Throwable t) {
-                callback.onError(new DataError(t.getCause().getLocalizedMessage()));
+                String errorMessage = t.getCause() != null ? t.getCause().getLocalizedMessage() : "Error on Request";
+                callback.onError(new DataError(errorMessage));
             }
         });
     }
@@ -69,7 +71,59 @@ public class AppNetworkRepository implements AppDataRepository {
 
             @Override
             public void onFailure(Call<App> call, Throwable t) {
-                callback.onError(new DataError(t.getCause().getLocalizedMessage()));
+                String errorMessage = t.getCause() != null ? t.getCause().getLocalizedMessage() : "Error on Request";
+                callback.onError(new DataError(errorMessage));
+            }
+        });
+    }
+
+    @Override
+    public void getRecommendedApps(Long offset, Long limit, final DataCallback<List<App>> callback) {
+        client.getRecommendedApps(offset, limit).enqueue(new Callback<AppResult>() {
+            @Override
+            public void onResponse(Call<AppResult> call, Response<AppResult> response) {
+                int responseCode = response.code();
+                switch (responseCode) {
+                    case 200:
+                        AppResult appResult = response.body();
+                        if (appResult != null && appResult.getResults() != null) {
+                            callback.onSuccess(appResult.getResults());
+                        } else {
+                            callback.onError(new DataError("Empty Body"));
+                        }
+                        break;
+                    default:
+                        callback.onError(new DataError("Error Status: " + responseCode));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AppResult> call, Throwable t) {
+                String errorMessage = t.getCause() != null ? t.getCause().getLocalizedMessage() : "Error on Request";
+                callback.onError(new DataError(errorMessage));
+            }
+        });
+    }
+
+    @Override
+    public void sendMyApps(List<PackageName> packageNames, final DataCallback<Void> callback) {
+        client.postMyApps(packageNames).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                int responseCode = response.code();
+                switch (responseCode) {
+                    case 200:
+                        callback.onSuccess(response.body());
+                        break;
+                    default:
+                        callback.onError(new DataError("Error Status: " + responseCode));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                String errorMessage = t.getCause() != null ? t.getCause().getLocalizedMessage() : "Error on Request";
+                callback.onError(new DataError(errorMessage));
             }
         });
     }
