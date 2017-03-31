7b55cc10f3d8ef3986ab9607b2298d8b81ba8dec
Bruno Kosawa <bkosawa@gmail.com>
2017-03-31 09:09:09 -0300

    Creating error type to manage not found error

--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/DataError.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/DataError.java
@@ -1,13 +1,37 @@
 package br.com.kosawalabs.apprecommendation.data;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class DataError {
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({CONNECTION_ERROR, NOT_FOUND})
+    public @interface ErrorCode {
+    }
+
+    public static final int CONNECTION_ERROR = 0;
+    public static final int NOT_FOUND = 404;
+
     private String cause;
+    @ErrorCode
+    private int errorCode = -1;
 
     public DataError(String cause) {
         this.cause = cause;
     }
 
+    public DataError(String cause, @ErrorCode int errorCode) {
+        this.cause = cause;
+        this.errorCode = errorCode;
+    }
+
     public String getCause() {
         return cause;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
     }
 }
