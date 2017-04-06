88c6e0518900183e01ceb479b629756084f7b92f
Bruno Kosawa <bkosawa@gmail.com>
2017-04-06 09:17:27 -0300

    Making object app a Parcelable

--- app/src/main/java/br/com/kosawalabs/apprecommendation/data/pojo/App.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/data/pojo/App.java
@@ -1,11 +1,13 @@
 package br.com.kosawalabs.apprecommendation.data.pojo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class App {
+public class App implements Parcelable {
     @SerializedName("id")
     @Expose
     private Integer id;
@@ -87,4 +89,53 @@ public class App {
     public String getSize() {
         return size;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.packageName);
+        dest.writeString(this.name);
+        dest.writeString(this.iconUrl);
+        dest.writeString(this.description);
+        dest.writeString(this.categoryKey);
+        dest.writeString(this.categoryName);
+        dest.writeString(this.developerName);
+        dest.writeString(this.version);
+        dest.writeString(this.contentRating);
+        dest.writeString(this.size);
+    }
+
+    public App() {
+    }
+
+    protected App(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.packageName = in.readString();
+        this.name = in.readString();
+        this.iconUrl = in.readString();
+        this.description = in.readString();
+        this.categoryKey = in.readString();
+        this.categoryName = in.readString();
+        this.developerName = in.readString();
+        this.version = in.readString();
+        this.contentRating = in.readString();
+        this.size = in.readString();
+    }
+
+    public static final Parcelable.Creator<App> CREATOR = new Parcelable.Creator<App>() {
+        @Override
+        public App createFromParcel(Parcel source) {
+            return new App(source);
+        }
+
+        @Override
+        public App[] newArray(int size) {
+            return new App[size];
+        }
+    };
 }
