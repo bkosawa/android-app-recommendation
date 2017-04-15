aade75b277f0942cc0f098aae24f1b174c93bca3
Bruno Kosawa <bkosawa@gmail.com>
2017-04-15 16:37:10 -0300

    Creating app list presenter from model contract

--- app/src/main/java/br/com/kosawalabs/apprecommendation/presentation/list/contract/AppListPresenter.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/presentation/list/contract/AppListPresenter.java
@@ -1,6 +1,10 @@
 package br.com.kosawalabs.apprecommendation.presentation.list.contract;
 
 
+import java.util.List;
+
+import br.com.kosawalabs.apprecommendation.data.pojo.App;
+
 public abstract class AppListPresenter {
     public interface AppListPresenterFromView {
         void init();
@@ -14,5 +18,17 @@ public abstract class AppListPresenter {
         void setRecommended(boolean isRecommended);
 
         boolean hasStopLoading();
+    }
+
+    public interface AppListPresenterFromModel {
+        void onResultApps(List<App> apps);
+
+        void onResultMoreApps(List<App> apps);
+
+        void onListNotFound();
+
+        void onRequestNotAllowed();
+
+        void onRequestError(String cause);
     }
 }
