2f53445910dcc78dd557b6eadc85ef86c6a6ea31
Bruno Kosawa <bkosawa@gmail.com>
2017-04-15 14:21:07 -0300

    Using cache on source image to improve animation

--- app/src/main/java/br/com/kosawalabs/apprecommendation/visual/ImageLoaderFacade.java
+++ app/src/main/java/br/com/kosawalabs/apprecommendation/visual/ImageLoaderFacade.java
@@ -5,13 +5,20 @@ import android.support.v4.app.Fragment;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class ImageLoaderFacade {
-    public static void loadImage(Fragment fragment, String url, ImageView imageView){
-        Glide.with(fragment).load(url).into(imageView);
+    public static void loadImage(Fragment fragment, String url, ImageView imageView) {
+        Glide.with(fragment)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(imageView);
     }
 
-    public static void loadImage(Activity activity, String url, ImageView imageView){
-        Glide.with(activity).load(url).into(imageView);
+    public static void loadImage(Activity activity, String url, ImageView imageView) {
+        Glide.with(activity)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(imageView);
     }
 }
